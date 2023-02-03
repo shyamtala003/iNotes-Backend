91df72bde0ac00d3af5afe9805a217bd16e2e0dd
shyamtala003 <shyamtala003@gmail.com>
2023-02-03 15:48:23 +0530

    build issue fix

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const connection = require("./db");
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 // for connect to mongodb
-connection();
+
 const Port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
+mongoose.set("strictQuery", false);
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(Port, () => {
-  console.log(`iNotes app listing at https://localhost:${Port}`);
-});
+let connectToMongo = async () => {
+  let databaseUrl = process.env.DATABASE_URL;
+  let connect = await mongoose.connect(databaseUrl);
+
+  if (connect) {
+    console.log("database succesully connected");
+    app.listen(Port, () => {
+      console.log(`iNotes app listing at http://localhost:${Port}`);
+    });
+  } else {
+    console.log(`error occurse : ${connect}`);
+    process.exit(1);
+  }
+};
+
+connectToMongo();
